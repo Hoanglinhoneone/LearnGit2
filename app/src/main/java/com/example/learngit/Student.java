797f0b86a930409a3082b962a3c33dcca444ca51
Hoanglinhoneone <hoangngoclinh305@gmail.com>
2024-05-26 22:47:29 +0700

    Update student getters

--- app/src/main/java/com/example/learngit/Student.java
+++ app/src/main/java/com/example/learngit/Student.java
@@ -3,4 +3,17 @@ package com.example.learngit;
 public class Student {
     private int name;
     private int gpa;
+    private String birthday;
+
+    public int getName() {
+        return name;
+    }
+
+    public int getGpa() {
+        return gpa;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
 }
