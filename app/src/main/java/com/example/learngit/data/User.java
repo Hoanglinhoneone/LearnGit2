419a66f22cdf66d819f6482ec594a4ba9bbc9839
Hoanglinhoneone <hoangngoclinh305@gmail.com>
2024-05-29 07:38:41 +0700

    Add User

---
+++ app/src/main/java/com/example/learngit/data/User.java
@@ -0,0 +1,10 @@
+package com.example.learngit.data;
+
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String midName;
+    private String email;
+    // this is an inline comment
+}
